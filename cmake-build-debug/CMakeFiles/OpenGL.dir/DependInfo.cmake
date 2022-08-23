948c6cd78ee7b5e8ae1e20bc020218c8dbe5b723
李圳熊 <joker.d.persona@gmail.com>
2022-08-23 13:54:52 +0800

    [Add] 初始化mac开发环境

---
+++ cmake-build-debug/CMakeFiles/OpenGL.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/lizhenxiong/OpenGL/glad.c" "/Users/lizhenxiong/OpenGL/cmake-build-debug/CMakeFiles/OpenGL.dir/glad.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  ".././include"
+  "/usr/local/include"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/lizhenxiong/OpenGL/main.cpp" "/Users/lizhenxiong/OpenGL/cmake-build-debug/CMakeFiles/OpenGL.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  ".././include"
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
